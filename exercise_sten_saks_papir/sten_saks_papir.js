21d1c9e5319f5fbfaa56d1dcee0ce7a3165cf9ef
Emilie <emer0005@stud.kea.dk>
2026-01-28 12:30:38 +0100

    Exercise sten saks papir

---
+++ exercise_sten_saks_papir/sten_saks_papir.js
@@ -0,0 +1,78 @@
+"use strict";
+const paper_btn = document.querySelector(".paper");
+const rock_btn = document.querySelector(".rock");
+const scissors_btn = document.querySelector(".scissors");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+const draw = document.querySelector("#draw");
+const win = document.querySelector("#win");
+const lose = document.querySelector("#lose");
+let userChoice;
+let computerChoice;
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  console.log("hej");
+  rock_btn.addEventListener("click", rockClick);
+  paper_btn.addEventListener("click", paperClick);
+  scissors_btn.addEventListener("click", scissorsClick);
+}
+
+function rockClick() {
+  console.log("rock");
+  userChoice = "rock";
+  computerChooses();
+  resetGame();
+}
+
+function paperClick() {
+  console.log("paper");
+  userChoice = "paper";
+  computerChooses();
+  resetGame();
+}
+
+function scissorsClick() {
+  console.log("scissors");
+  userChoice = "scissors";
+  computerChooses();
+  resetGame();
+}
+
+function computerChooses() {
+  const Selection = ["rock", "paper", "scissors"];
+  let index = Math.floor(Math.random() * 2);
+  computerChoice = Selection[index];
+  handShakesStart();
+}
+
+function handShakesStart() {
+  player1.classList.add("shake");
+  player2.classList.add("shake");
+  player1.addEventListener("animationend", showChoice);
+}
+
+function showChoice() {
+  console.log("choice");
+  player1.classList.remove("shake");
+  player2.classList.remove("shake");
+  player1.classList.add(userChoice);
+  player2.classList.add(computerChoice);
+
+  if (userChoice === computerChoice) {
+    draw.classList.remove("hidden");
+  } else if ((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" && computerChoice === "paper")) {
+    win.classList.remove("hidden");
+  } else {
+    lose.classList.remove("hidden");
+  }
+}
+
+function resetGame() {
+  win.classList.add("hidden");
+  lose.classList.add("hidden");
+  draw.classList.add("hidden");
+  player1.classList.remove("rock", "paper", "scissors");
+  player2.classList.remove("rock", "paper", "scissors");
+}
